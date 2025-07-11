988bf8cf7af2c0b48201011162958c54acff8428
Egor Egorov <lordero@list.ru>
2025-07-12 00:40:51 +0500

    Приложение Payment Service на основе Spring Boot

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+J
+simple-http-server/pom.xml,2\4\247be31907f8cc117b6fc30a84305a063938c32c
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+T
+$simple-http-server/static/index.html,a\9\a961d7b7091a190a7b77a0cfe99d7782352e16c6
+T
+$simple-http-server/static/hello.html,e\c\eccd7493d2a2a19c267b9672edce73d8b4be332b
+g
+7simple-http-server/src/main/java/iprody/HttpServer.java,d\7\d70555a8acda1df314030895d0eea4bfab823aa9
+9
+	README.MD,9\6\968e80af9562e09cebe594968b130076952eafe1
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.iprody.payment.service.app.controller;
+import com.iprody.payment.service.app.model.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    private final Payment payment = new Payment(1L, 99.99);
+    private final Map<Long, Payment> payments = fillPayments();
+
+    @GetMapping
+    public  Map<Long, Payment> getPayments() {
+        return payments;
+    }
+
+    @GetMapping("/{id}")
+    public Payment getPayment(@PathVariable("id") Long id){
+        return payments.get(id)!=null ? payments.get(id) : null;
+    }
+
+    private Map<Long, Payment> fillPayments(){
+        return Map.of(1L, new Payment(1,90), 2L, new Payment(2,45),3L, new Payment(3,20));
+    }
+}
