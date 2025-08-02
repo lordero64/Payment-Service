f626c68a63f6f9508956a7942cad040b1041cd73
Egor Egorov <lordero@list.ru>
2025-08-02 23:04:30 +0500

    PaymentFilterFactory

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentFilterFactory.java
@@ -0,0 +1,41 @@
+package com.iprody.payment.service.app.persistence;
+
+import com.iprody.payment.service.app.persistence.entity.Payment;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+public final class PaymentFilterFactory {
+    public static final Specification<Payment> EMPTY = (root, query, cb) -> null;
+
+    public static Specification<Payment> fromFilter(PaymentFilter paymentFilter) {
+        Specification<Payment> spec = EMPTY;
+
+        if(paymentFilter.getStatus() != null){
+            spec = spec.and(PaymentSpecifications.hasStatus(paymentFilter.getStatus()));
+        }
+
+        if (StringUtils.hasText(paymentFilter.getCurrency())) {
+            spec = spec.and(PaymentSpecifications.hasCurrency(paymentFilter.getCurrency()));
+        }
+
+        if (paymentFilter.getMinAmount() != null && paymentFilter.getMaxAmount() != null) {
+            spec = spec.and(PaymentSpecifications.amountBetween(
+                    paymentFilter.getMinAmount(), paymentFilter.getMaxAmount()));
+        }
+
+        if (paymentFilter.getCreatedAfter() != null && paymentFilter.getCreatedBefore() != null) {
+            spec = spec.and(PaymentSpecifications.createdBetween(
+                    paymentFilter.getCreatedAfter(), paymentFilter.getCreatedBefore()));
+        }
+
+        if(paymentFilter.getCreatedAfter() != null){
+            spec = spec.and((PaymentSpecifications.createdAtAfter(paymentFilter.getCreatedAfter())));
+        }
+
+        if(paymentFilter.getCreatedBefore() != null){
+            spec = spec.and((PaymentSpecifications.createdAtBefore(paymentFilter.getCreatedBefore())));
+        }
+
+        return spec;
+    }
+}
