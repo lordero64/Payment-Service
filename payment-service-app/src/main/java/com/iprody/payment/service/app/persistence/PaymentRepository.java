94716a59cd32f5ed75f157281cb0a24ac550474d
Egor Egorov <lordero@list.ru>
2025-08-04 19:04:19 +0500

    checkstyle fix

--- payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentRepository.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentRepository.java
@@ -9,5 +9,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 import java.util.UUID;
 public interface PaymentRepository extends JpaRepository<Payment, UUID>, JpaSpecificationExecutor<Payment> {
+
     List<Payment> findByStatus(PaymentStatus status);
 }
