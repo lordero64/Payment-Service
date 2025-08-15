4a1dab8398a3b3f71b39bbe2eb5582b8ba161627
Egor Egorov <lordero@list.ru>
2025-08-15 16:07:34 +0500

    PaymentController

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -2,14 +2,19 @@ package com.iprody.payment.service.app.controller;
 
 import com.iprody.payment.service.app.persistence.PaymentFilter;
 import com.iprody.payment.service.app.persistence.entity.PaymentStatus;
+import com.iprody.payment.service.app.services.NoteUpdateDto;
 import com.iprody.payment.service.app.services.PaymentDto;
 import com.iprody.payment.service.app.services.PaymentService;
+import com.iprody.payment.service.app.services.PaymentStatusUpdateDto;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 import java.util.UUID;
@@ -21,8 +26,8 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
-    @GetMapping("/all")
-    public Page<PaymentDto> getAll(@ModelAttribute PaymentFilter paymentFilter,
+    @GetMapping()
+    public Page<PaymentDto> search(@ModelAttribute PaymentFilter paymentFilter,
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "5") int size,
         @RequestParam(defaultValue = "guid") String sortBy,
@@ -32,18 +37,47 @@ public class PaymentController {
             : Sort.by(sortBy).ascending();
 
         final Pageable pageable = PageRequest.of(page, size, sort);
-        return paymentService.findAll(paymentFilter, pageable);
+        return paymentService.search(paymentFilter, pageable);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PaymentDto create(@RequestBody PaymentDto dto) {
+        return paymentService.create(dto);
     }
 
     @GetMapping("/{guid}")
-    public PaymentDto getById(@PathVariable UUID guid) {
-        return  paymentService.findById(guid);
+    public PaymentDto get(@PathVariable UUID guid) {
+        return  paymentService.get(guid);
+    }
+
+    @PutMapping("/{id}")
+    public PaymentDto update(@PathVariable UUID id, @RequestBody PaymentDto dto) {
+        return paymentService.update(id, dto);
+    }
+
+    @PatchMapping("/{id}/status")
+    public void updateStatus(
+        @PathVariable UUID id,
+        @RequestBody @Valid PaymentStatusUpdateDto dto) {
+        paymentService.updateStatus(id, dto.getStatus());
+    }
+
+    @PatchMapping("/{id}/note")
+    public void updateNote(
+        @PathVariable UUID id,
+        @RequestBody @Valid NoteUpdateDto dto) {
+        paymentService.updateNote(id, dto.getNote());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID id) {
+        paymentService.delete(id);
     }
 
     @GetMapping("/by_status/{status}")
     public List<PaymentDto> getByStatus(@PathVariable PaymentStatus paymentStatus) {
         return paymentService.getByStatus(paymentStatus);
     }
-
-
 }
