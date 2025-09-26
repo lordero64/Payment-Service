0f659bf4c23ba5d7e19237c7bf89c296fd6fcb05
Egor Egorov <lordero@list.ru>
2025-09-26 19:38:52 +0500

    AbstractIntegrationTest rename

--- payment-service-app/src/test/java/com/iprody/payment/service/app/AbstractIntegrationTest.java
+++ payment-service-app/src/test/java/com/iprody/payment/service/app/AbstractIntegrationTest.java
@@ -3,17 +3,19 @@ package com.iprody.payment.service.app;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.containers.wait.strategy.WaitAllStrategy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.Duration;
 
 @SpringBootTest
 @Testcontainers
-public class AbstractPostgresIntegrationTest {
+public class AbstractIntegrationTest {
     @Container
     protected static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("payment-db")
@@ -26,11 +28,19 @@ public class AbstractPostgresIntegrationTest {
             )
             .withStartupTimeout(Duration.ofSeconds(200));
 
+    @Container
+    static final KafkaContainer KAFKA = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"))
+            .withKraft()
+            .withStartupTimeout(Duration.ofSeconds(120));
+
     @DynamicPropertySource
     static void overrideProps(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
         registry.add("spring.liquibase.change-log", () -> "classpath:/db/changelog/master-test-changelog.yaml");
+
+        //Настройка Kafka
+        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
     }
 }
