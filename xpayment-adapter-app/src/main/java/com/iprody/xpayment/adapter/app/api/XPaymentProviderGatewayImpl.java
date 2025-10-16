baf4aa7addbf63e7392dd7221fdbd022b6fc5009
Egor Egorov <lordero@list.ru>
2025-10-16 22:50:57 +0500

    XPaymentProviderGatewayImpl

--- xpayment-adapter-app/src/main/java/com/iprody/xpayment/adapter/app/api/XPaymentProviderGatewayImpl.java
+++ xpayment-adapter-app/src/main/java/com/iprody/xpayment/adapter/app/api/XPaymentProviderGatewayImpl.java
@@ -2,23 +2,41 @@ package com.iprody.xpayment.adapter.app.api;
 
 import com.iprody.xpayment.adapter.app.api.client.DefaultApi;
 import com.iprody.xpayment.adapter.app.api.model.ChargeResponse;
-import com.iprody.xpayment.adapter.app.api.model.CreateChargeRequest;
+import com.iprody.xpayment.adapter.app.mapper.CreateChargeRequestMapper;
+import com.iprody.xpayment.adapter.app.mapper.CreateChargeResponseMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import java.util.UUID;
+
 @Service
 class XPaymentProviderGatewayImpl implements XPaymentProviderGateway {
     private final DefaultApi defaultApi;
-    public XPaymentProviderGatewayImpl(DefaultApi defaultApi) {
+    private final CreateChargeResponseMapper responseMapper;
+    private final CreateChargeRequestMapper requestMapper;
+
+    public XPaymentProviderGatewayImpl(DefaultApi defaultApi, CreateChargeResponseMapper responseMapper, CreateChargeRequestMapper requestMapper) {
         this.defaultApi = defaultApi;
+        this.responseMapper = responseMapper;
+        this.requestMapper = requestMapper;
     }
+
     @Override
-    public ChargeResponse createCharge(CreateChargeRequest createChargeRequest) throws RestClientException {
-            return defaultApi.createCharge(createChargeRequest);
+    public CreateChargeResponseDto createCharge(CreateChargeRequestDto createChargeRequestDto) throws RestClientException {
+        return responseMapper.toEntity( defaultApi.createCharge(requestMapper.toEntity(createChargeRequestDto)));
     }
 
     @Override
     public ChargeResponse retrieveCharge(UUID id) throws RestClientException {
-            return defaultApi.retrieveCharge(id);
+        return null;
     }
+
+//    @Override
+//    public CreateChargeResponseDto createCharge(CreateChargeRequestDto createChargeRequest) throws RestClientException {
+//        return defaultApi.createCharge((createChargeRequest).map(CreateChargeRequestMapper::toDto).orElseThrow());
+//    }
+//
+//    @Override
+//    public CreateChargeResponseDto retrieveCharge(UUID id) throws RestClientException {
+//        return defaultApi.retrieveCharge(id);
+//    }
 }
