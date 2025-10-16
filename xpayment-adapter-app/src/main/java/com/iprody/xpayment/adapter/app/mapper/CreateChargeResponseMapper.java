c332195752326d53b534a4cb7f2ed9d8f877a2d6
Egor Egorov <lordero@list.ru>
2025-10-17 00:01:49 +0500

    CreateChargeResponseMapper

--- xpayment-adapter-app/src/main/java/com/iprody/xpayment/adapter/app/mapper/CreateChargeResponseMapper.java
+++ xpayment-adapter-app/src/main/java/com/iprody/xpayment/adapter/app/mapper/CreateChargeResponseMapper.java
@@ -3,12 +3,9 @@ package com.iprody.xpayment.adapter.app.mapper;
 import com.iprody.xpayment.adapter.app.api.CreateChargeResponseDto;
 import com.iprody.xpayment.adapter.app.api.model.ChargeResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring",
-        unmappedSourcePolicy = ReportingPolicy.ERROR,
-        unmappedTargetPolicy = ReportingPolicy.ERROR
-)
+
+@Mapper(componentModel = "spring")
 public interface CreateChargeResponseMapper {
     ChargeResponse toDto(CreateChargeResponseDto CreateChargeResponseDto);
 
